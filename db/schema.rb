347e7f6de18fefcff6d2aa017a2152b3d5a9c61a
Zhuo-Fei Hui <fei@blinkist.com>
2018-09-11 13:10:17 +0200

    Changed db to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_11_100841) do
-
-  create_table "services", force: :cascade do |t|
-    t.string "name"
-    t.boolean "blocked", default: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
